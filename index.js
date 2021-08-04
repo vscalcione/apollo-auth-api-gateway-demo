1981ee89bbf892bbd3279baab2425061b4858534
vscalcione <vincenzo99.scalcione@gmail.com>
2021-08-04 13:13:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const express = require('express');
+const expressJwt = require('express-jwt');
+const dotenv = require('dotenv').config();
+
+const createApolloGateway = async () => {
+    const port = 4000;
+    const app = express();
+    app.use(
+        expressJwt({
+            secret: process.env.SECRET_KEY,
+            algorithms: ["H256"],
+            credentialsRequired: false
+        })
+    );
+
+    const gateway = new ApolloGateway({
+        serviceList: [{ name: 'accounts', url: 'http://localhost:4001'}],
+        buildService({ name, url }) {
+            return new RemoteGraphQLDataSource({
+                url,
+                willSendRequest({ request, context }) {
+                    request.http.headers.set(
+                        'user',
+                        context.user ? JSON.stringify(context.user) : null
+                    );
+                }
+            });
+        }
+    });
+
+    const server = new ApolloServer({
+        gateway,
+        subscribtions: false,
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground({
+            //options
+        })],
+        context: ({ req }) => {
+            const user = req.user || null;
+            console.log(user);
+            return { user };
+        }
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
+    app.listen({ port }, () => {
+        console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+    });
+
+    console.log('here');
+    return { server, app };
+}
+
+createApolloGateway().then(({ server, app }) => console.log('done'));
